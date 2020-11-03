c9ce755998fdde7966c1bf6eea83600622ead912
syaheerLGD <73862545+syaheerLGD@users.noreply.github.com>
2020-11-03 14:16:26 +0800

    Create userinput.c

---
+++ userinput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void) {
+	//this is my second program in C
+	int age;
+	printf("Hi, How old are you? > ");
+	scanf("%d", &age);
+	printf("You are %d years old", age);
+	return (0);
+}
